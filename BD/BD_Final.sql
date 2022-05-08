64171758bcf222e1544a4cbd04a8908d843e619e
Manuel-Coca <manuel.cocaalba@alum.uca.es>
2022-05-08 19:29:31 +0200

    BDFinal

--- BD/BD_Final.sql
+++ BD/BD_Final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-05-2022 a las 17:35:50
+-- Tiempo de generación: 08-05-2022 a las 19:29:11
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -102,19 +102,21 @@ CREATE TABLE `peliculas` (
   `estreno` date NOT NULL,
   `director` varchar(50) NOT NULL,
   `portada` text NOT NULL,
-  `sinopsis` text DEFAULT NULL
+  `sinopsis` text DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `peliculas`
 --
 
-INSERT INTO `peliculas` (`id`, `duracion`, `titulo`, `genero`, `estreno`, `director`, `portada`, `sinopsis`) VALUES
-(1, 126, 'Doctor Strange en el Multiverso de la Locura\n', 'Fantasía', '2022-05-05', 'Sam Raimi', 'https://i.ibb.co/ZgJ26RR/doctor-strange-in-the-multiverse-of-madness-4pjr.png', 'En esta nueva aventura, el Doctor Strange (Benedict Cumberbatch) va a poner a prueba los límites de sus poderes y esto le llevará a explorar una nueva dimensión de sus capacidades. Tras haber fallado su hechizo, cuando trataba de ayudar a Spider-Man (Tom Holland) con su identidad secreta,  tiene que recurrir a una vieja amiga, Wanda Maximoff (Elizabeth Olsen) para enmendar su error. Como nunca antes, el famoso hechicero de Marvel explorará los oscuros rincones del Multiverso, donde deberá contar con nuevos y viejos aliados si quiere sobrevivir a las peligrosas realidades alternativas del universo y enfrentarse a un nuevo misterioso enemigo.'),
-(2, 144, 'Animales Fantásticos: Los Secretos de Dumbledore\n', 'Aventura', '2022-04-08', 'David Yates', 'https://i.ibb.co/s5zrR23/0138242.jpg', 'En Animales Fantásticos: Los Secretos de Dumbledore el malvado y poderoso mago Grindelwald sigue buscando adeptos a su causa, pero esta vez se transportará a todos los rincones y mundos mágicos existentes para obtener lo que quiere. Por otra parte, Dumbledore unirá a su propio ejército formado por Newt y Theseus Scamander, Jacob, entre otros, para poner fin a la guerra que está a punto de comenzar Grindelwald en su nombre. \n\nMientras encuentran aliados y la manera de poner fin a esta oscura edad, el pasado de Dumbledore irá cada vez tomando más presencia en la historia entre él y su antiguo amigo Grindelwald, con quien le unen otros grandes lazos más allá de la enemistad.'),
-(3, 105, 'Morbius', 'Accion', '2022-04-01', 'Daniel Espinosa', 'https://i.ibb.co/t45tNWg/MRBS-INTL-On-Line-6072x9000-Montage-06-traducido.jpg', 'El Doctor Michael Morbius (Jared Leto) es un bioquímico que sufre una extraña enfermedad en la sangre. Para curarse y dar una respuesta a su trastorno, en el proceso, se infecta sin darse cuenta con una forma de vampirismo. Aunque debería haber muerto, tras la cura, Michael se siente más vivo que nunca y adquiere dones como fuerza, velocidad, capacidad de ecolocalización, además de una necesidad irresistible de consumir sangre. Trágicamente convertido en un imperfecto antihéroe, el Doctor Morbius tendrá una última oportunidad, pero ¿a qué precio?\n\nSpin-off de uno de los villanos más conocidos de Spider-Man, creado por Roy Thomas y Gil Kane, el filme lo producen los responsables de Spider-Man: Homecoming (2017), Venom (2018) y Spider-Man: Lejos de casa (2019).'),
-(4, 99, 'Sonic: La película', 'Aventura', '2022-05-06', 'Jeff Fowler', 'https://i.ibb.co/BTF5XmT/0815514.jpg', 'Sonic, el famoso erizo azul de SEGA vivirá su primera aventura en la pantalla grande en esta película, que incluirá tanto acción real como animación creada por ordenador. En esta adaptación cinematográfica basada en la conocida saga de videojuegos descubriremos a su protagonista: Sonic, el erizo con la habilidad de correr a la velocidad del sonido. En esta nueva aventura, el erizo azul deberá viajar hasta nuestro mundo para poder salvar el suyo. En su misión no estará solo, pues contará con la ayuda de su amigo humano Tom Wachowski (James Marsden), con quien intentará escapar del gobierno que quiere capturarlo, además de huir de las artimañas del villano Doctor Ivo Robonik (Jim Carrey).'),
-(5, 116, 'Uncharted: Fuera del Mapa', 'Aventura / Acción', '2022-02-11', 'Ruben Fleischer', 'https://i.ibb.co/ccyTjDg/UNCH-LAS-On-Line-TSR-1400x2100-Daisy-Chain-07.jpg', 'Basado en uno de los juegos más aclamados por la critica y que más ha vendido en la historia, Uncharted: Fuera del mapa, narra la historia de Nathan Drake y la primer aventura con su socio-rival Victor “Sully” Sullivan. Estelarizada por Tom Holland como Nathan Drake y Mark Wahlberg como el bromista de intelecto rápido Sully. Uncharted: Fuera del mapa presenta al público la manera en que Nathan Drake se convierte en un cazador de tesoros mientras que resuelve los más grandes misterios en una aventura llena de aventura y acción épica alrededor del mundo.');
+INSERT INTO `peliculas` (`id`, `duracion`, `titulo`, `genero`, `estreno`, `director`, `portada`, `sinopsis`, `updated_at`, `created_at`) VALUES
+(1, 126, 'Doctor Strange en el Multiverso de la Locura\n', 'Fantasía', '2022-05-05', 'Sam Raimi', 'https://i.ibb.co/ZgJ26RR/doctor-strange-in-the-multiverse-of-madness-4pjr.png', 'En esta nueva aventura, el Doctor Strange (Benedict Cumberbatch) va a poner a prueba los límites de sus poderes y esto le llevará a explorar una nueva dimensión de sus capacidades. Tras haber fallado su hechizo, cuando trataba de ayudar a Spider-Man (Tom Holland) con su identidad secreta,  tiene que recurrir a una vieja amiga, Wanda Maximoff (Elizabeth Olsen) para enmendar su error. Como nunca antes, el famoso hechicero de Marvel explorará los oscuros rincones del Multiverso, donde deberá contar con nuevos y viejos aliados si quiere sobrevivir a las peligrosas realidades alternativas del universo y enfrentarse a un nuevo misterioso enemigo.', '2022-05-08 16:49:48', '2022-05-08 16:49:48'),
+(2, 144, 'Animales Fantásticos: Los Secretos de Dumbledore\n', 'Aventura', '2022-04-08', 'David Yates', 'https://i.ibb.co/s5zrR23/0138242.jpg', 'En Animales Fantásticos: Los Secretos de Dumbledore el malvado y poderoso mago Grindelwald sigue buscando adeptos a su causa, pero esta vez se transportará a todos los rincones y mundos mágicos existentes para obtener lo que quiere. Por otra parte, Dumbledore unirá a su propio ejército formado por Newt y Theseus Scamander, Jacob, entre otros, para poner fin a la guerra que está a punto de comenzar Grindelwald en su nombre. \n\nMientras encuentran aliados y la manera de poner fin a esta oscura edad, el pasado de Dumbledore irá cada vez tomando más presencia en la historia entre él y su antiguo amigo Grindelwald, con quien le unen otros grandes lazos más allá de la enemistad.', '2022-05-08 16:49:48', '2022-05-08 16:49:48'),
+(3, 105, 'Morbius', 'Accion', '2022-04-01', 'Daniel Espinosa', 'https://i.ibb.co/t45tNWg/MRBS-INTL-On-Line-6072x9000-Montage-06-traducido.jpg', 'El Doctor Michael Morbius (Jared Leto) es un bioquímico que sufre una extraña enfermedad en la sangre. Para curarse y dar una respuesta a su trastorno, en el proceso, se infecta sin darse cuenta con una forma de vampirismo. Aunque debería haber muerto, tras la cura, Michael se siente más vivo que nunca y adquiere dones como fuerza, velocidad, capacidad de ecolocalización, además de una necesidad irresistible de consumir sangre. Trágicamente convertido en un imperfecto antihéroe, el Doctor Morbius tendrá una última oportunidad, pero ¿a qué precio?\n\nSpin-off de uno de los villanos más conocidos de Spider-Man, creado por Roy Thomas y Gil Kane, el filme lo producen los responsables de Spider-Man: Homecoming (2017), Venom (2018) y Spider-Man: Lejos de casa (2019).', '2022-05-08 16:49:48', '2022-05-08 16:49:48'),
+(4, 99, 'Sonic: La película', 'Aventura', '2022-05-06', 'Jeff Fowler', 'https://i.ibb.co/BTF5XmT/0815514.jpg', 'Sonic, el famoso erizo azul de SEGA vivirá su primera aventura en la pantalla grande en esta película, que incluirá tanto acción real como animación creada por ordenador. En esta adaptación cinematográfica basada en la conocida saga de videojuegos descubriremos a su protagonista: Sonic, el erizo con la habilidad de correr a la velocidad del sonido. En esta nueva aventura, el erizo azul deberá viajar hasta nuestro mundo para poder salvar el suyo. En su misión no estará solo, pues contará con la ayuda de su amigo humano Tom Wachowski (James Marsden), con quien intentará escapar del gobierno que quiere capturarlo, además de huir de las artimañas del villano Doctor Ivo Robonik (Jim Carrey).', '2022-05-08 16:49:48', '2022-05-08 16:49:48'),
+(5, 116, 'Uncharted: Fuera del Mapa', 'Aventura / Acción', '2022-02-11', 'Ruben Fleischer', 'https://i.ibb.co/ccyTjDg/UNCH-LAS-On-Line-TSR-1400x2100-Daisy-Chain-07.jpg', 'Basado en uno de los juegos más aclamados por la critica y que más ha vendido en la historia, Uncharted: Fuera del mapa, narra la historia de Nathan Drake y la primer aventura con su socio-rival Victor “Sully” Sullivan. Estelarizada por Tom Holland como Nathan Drake y Mark Wahlberg como el bromista de intelecto rápido Sully. Uncharted: Fuera del mapa presenta al público la manera en que Nathan Drake se convierte en un cazador de tesoros mientras que resuelve los más grandes misterios en una aventura llena de aventura y acción épica alrededor del mundo.', '2022-05-08 16:49:48', '2022-05-08 16:49:48');
 
 -- --------------------------------------------------------
 
@@ -165,24 +167,25 @@ CREATE TABLE `sesiones` (
   `id_pelicula` int(11) NOT NULL,
   `hora_inicio` time NOT NULL,
   `hora_fin` time NOT NULL,
-  `dia` date NOT NULL DEFAULT current_timestamp()
+  `dia` date NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `sesiones`
 --
 
-INSERT INTO `sesiones` (`id_sesion`, `id_sala`, `id_pelicula`, `hora_inicio`, `hora_fin`, `dia`) VALUES
-(4, 1, 1, '15:00:00', '17:00:06', '2022-05-10'),
-(5, 2, 1, '20:00:00', '22:00:06', '2022-05-10'),
-(6, 1, 2, '12:00:00', '14:24:00', '2022-05-11'),
-(7, 2, 2, '17:00:00', '19:24:17', '2022-05-11'),
-(8, 1, 3, '17:00:54', '18:40:54', '2022-05-17'),
-(9, 1, 3, '19:00:54', '20:40:54', '2022-05-18'),
-(10, 1, 4, '15:00:14', '16:30:14', '2022-05-13'),
-(11, 2, 4, '17:30:14', '19:00:14', '2022-05-20'),
-(12, 1, 5, '17:31:14', '19:41:14', '2022-05-23'),
-(13, 2, 5, '20:31:14', '22:40:14', '2022-05-25');
+INSERT INTO `sesiones` (`id_sesion`, `id_sala`, `id_pelicula`, `hora_inicio`, `hora_fin`, `dia`, `updated_at`, `created_at`) VALUES
+(4, 1, 1, '15:00:00', '17:00:06', '2022-05-10', '2022-05-08 17:11:33', '2022-05-08 17:11:33'),
+(5, 2, 1, '20:00:00', '22:00:06', '2022-05-10', '2022-05-08 17:11:33', '2022-05-08 17:11:33'),
+(6, 1, 2, '12:00:00', '14:24:00', '2022-05-11', '2022-05-08 17:11:33', '2022-05-08 17:11:33'),
+(7, 2, 2, '17:00:00', '19:24:17', '2022-05-11', '2022-05-08 17:11:33', '2022-05-08 17:11:33'),
+(8, 1, 3, '17:00:54', '18:40:54', '2022-05-17', '2022-05-08 17:11:33', '2022-05-08 17:11:33'),
+(9, 1, 3, '19:00:54', '20:40:54', '2022-05-18', '2022-05-08 17:11:33', '2022-05-08 17:11:33'),
+(10, 1, 4, '15:00:14', '16:30:14', '2022-05-13', '2022-05-08 17:11:33', '2022-05-08 17:11:33'),
+(11, 2, 4, '17:30:14', '19:00:14', '2022-05-20', '2022-05-08 17:11:33', '2022-05-08 17:11:33'),
+(12, 1, 5, '17:31:14', '19:41:14', '2022-05-23', '2022-05-08 17:11:33', '2022-05-08 17:11:33');
 
 -- --------------------------------------------------------
 
@@ -204,7 +207,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('WI5X8FpvcP50lAhDCFlIWye7srLOFu2sw8xdlZJR', 6, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoicmVFeTJzV0I0ckdEbTBKSDl5UDdZTEpsdTlKeVRGTnFHODR5enp4UiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjg6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9wZXJmaWwiO31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aTo2O30=', 1652024076);
+('WI5X8FpvcP50lAhDCFlIWye7srLOFu2sw8xdlZJR', 6, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoicmVFeTJzV0I0ckdEbTBKSDl5UDdZTEpsdTlKeVRGTnFHODR5enp4UiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9hZG1pbiI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjY7fQ==', 1652030842);
 
 -- --------------------------------------------------------
 
@@ -335,7 +338,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `personal_access_tokens`
@@ -347,13 +350,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT de la tabla `sesiones`
 --
 ALTER TABLE `sesiones`
-  MODIFY `id_sesion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_sesion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
